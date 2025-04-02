a6dd8aff1c83690ab526f61e074b96ea908d9403
SHAMANTA GARAI <shamanta_gaids2024@msit.edu.in>
2025-04-03 00:47:10 +0530

    script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll(".btn");
   buttons.forEach((button) => {
     button.addEventListener("click", function () {
-      alert(`You clicked on ${this.innerText}`);
+     
     });
   });
 
   const forgotBtn = document.querySelector(".forgot-btn");
   if (forgotBtn) {
     forgotBtn.addEventListener("click", function () {
-      alert("Forgot Password clicked!");
+     
     });
   }
 });
