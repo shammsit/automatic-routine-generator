dcc7d2b434dd29419363abd860ab37f02c9e1376
SHAMANTA GARAI <shamanta_gaids2024@msit.edu.in>
2025-04-03 00:48:59 +0530

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const buttons = document.querySelectorAll(".btn");
-  buttons.forEach((button) => {
-    button.addEventListener("click", function () {
-     
+  const signupBtn = document.querySelector(".signup-btn"); // Select Sign Up button
+  if (signupBtn) {
+    signupBtn.addEventListener("click", function () {
+      window.location.href = "signup.html"; // Redirect to signup page
     });
-  });
+  }
 
   const forgotBtn = document.querySelector(".forgot-btn");
   if (forgotBtn) {
     forgotBtn.addEventListener("click", function () {
-     
+      window.location.href = "forgot-password.html"; // Redirect to forgot password page
     });
   }
 });
